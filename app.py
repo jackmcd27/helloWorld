0ebab5e059c81d73b23b040d26cddf796c08ebcf
jackmcdonough <jackmcd1800@gmail.com>
2023-02-06 20:02:24 -0500

    Added some text changes illustrating this is my first update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Jack McDonough!'
+    return 'Hello World from Jack McDonough!This is my first code update'
 
 
 if __name__ == '__main__':
