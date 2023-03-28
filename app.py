36737dcae28a27f15a94ae76375cfaeebbaddbc5
jackmcdonough <jackmcd1800@gmail.com>
2023-03-27 20:28:44 -0400

    initial changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,5 +12,11 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+@app.route('/favorite-course')
+def favoritecourse():  # put application's code here
+    print('You entered your favorite course as:' + request.args.get('subject') + request.args.get('course_num'))
+    return render_template('favorite-course.html')
+
+
 if __name__ == '__main__':
     app.run()
